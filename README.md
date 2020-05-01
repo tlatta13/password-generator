ce424e9834e6a951eda5a230d2828f2064e059f2
tlatta13 <tlatta1313@gmail.com>
2020-04-30 23:00:28 -0400

    Files updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # password-generator
+
+
+## Needs
+* prompt to enter password length between 9 and 123 characters
+* prompt to allow special characters
+* prompt to allow numbers
+* 4 arrays for - upper case, lower case, numbers and special characters
+
+* if (i != 8) then reprompt password length
+* if (special character: true && numbers: true) then generate password using all arrays
+*    else if (special characters: false && numbers: true) then generate password without special character array
+*    else if (special characters: true && numbers: false) then generate password without number array
+*    else if (special characters: false && numbers: false) then generate password without special character or number array
--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-// Assignment Code
+
+passwordText.value = password;   
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+var allowSpecial = confirm("Allow special characters?");
+var allowUpper = confirm("Allow uppercase letters?");
+var allowLower = confirm("Allow lowercase letters?");
+var allowNum = confirm("Allow numbers in your password?");
+var passChar = parseInt(prompt("How long would you like your password to be?"));
 
 
+// Character Arrays
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "/", "?", "@", "^", "`", "~"];
+
+var choices = [];
+
+if (allowSpecial) {
+    choices.push(specialChar);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (allowUpper) {
+    choices.push(upperCase);
+}
+
+if (allowLower) {
+    choices.push(lowerCase);
+}
+
+if (allowNum) {
+    choices.push (numbers)
+}
+
+if (passChar < 8) {
+    alert("Password must be least 8 characters long")
+}  
+
+if (passChar >= 128) {
+    alert("Password must be less than 128 characters")
+}
+
+var password = "";
+
+for (var i=0; i < passChar; i++) {
+    var choice = choices[Math.floor(Math.random() * choices.length)];
+    var random = choice[Math.floor(Math.random() * choice.length)];
+    password += random;
+    password === choice
+}
+
+console.log(password);
+return password
+
+
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+document.getElementById("generateBtn").addEventListener("click", writePassword);
+}
+
+
